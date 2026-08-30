4fe8b16e54c387e0699758d6c7a8d1480905594d
FelRod-Diecast <nnbwb8g579@privaterelay.appleid.com>
2026-08-30 17:27:50 -0500

    Implement product loading and alert sending
    Added functions to load and save products from a JSON file. Implemented product alert sending functionality.

--- index-v2.js
+++ index-v2.js
@@ -1,13 +1,50 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
+const path = require("path");
+const PRODUCTS_FILE = path.join(__dirname, "products.json");
+ 
+function loadProducts() {
+try {
+return JSON.parse(fs.readFileSync(PRODUCTS_FILE, "utf8"));
+} catch {
+return {};
+}
+}
+ 
+function saveProducts(products) {
+fs.writeFileSync(
+PRODUCTS_FILE,
+JSON.stringify(products, null, 2)
+);
+}
+ 
+const products = loadProducts();
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
 const CHANNEL_ID = process.env.DROPS_CHANNEL_ID;
+async function sendProductAlert(product) {
+  const channel = await client.channels.fetch(CHANNEL_ID);
 
+  await channel.send({
+    content:
+      `🔥 NEW POKÉMON PRODUCT\n\n` +
+      `Store: ${product.store}\n` +
+      `Product: ${product.name}\n` +
+      `URL: ${product.url}`
+  });
+}
 client.once("clientReady", async () => {
+  console.log("PokemonTrackerV2 Online");
+
+  await sendProductAlert({
+    store: "TEST",
+    name: "Pokemon Black Bolt ETB",
+    url: "https://example.com"
+  });
+});
   console.log("PokemonTrackerV2 Online");
 
   // Target Scanner
