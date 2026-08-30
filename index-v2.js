e522d2b2288c9d5e50051509381ebe5d42eff302
FelRod-Diecast <nnbwb8g579@privaterelay.appleid.com>
2026-08-30 17:37:47 -0500

    Refactor index-v2.js for improved readability

--- index-v2.js
+++ index-v2.js
@@ -1,23 +1,24 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
+
 const PRODUCTS_FILE = path.join(__dirname, "products.json");
- 
+
 function loadProducts() {
-try {
-return JSON.parse(fs.readFileSync(PRODUCTS_FILE, "utf8"));
-} catch {
-return {};
+  try {
+    return JSON.parse(fs.readFileSync(PRODUCTS_FILE, "utf8"));
+  } catch {
+    return {};
+  }
 }
-}
- 
+
 function saveProducts(products) {
-fs.writeFileSync(
-PRODUCTS_FILE,
-JSON.stringify(products, null, 2)
-);
+  fs.writeFileSync(
+    PRODUCTS_FILE,
+    JSON.stringify(products, null, 2)
+  );
 }
- 
+
 const products = loadProducts();
 
 const client = new Client({
@@ -25,6 +26,7 @@ const client = new Client({
 });
 
 const CHANNEL_ID = process.env.DROPS_CHANNEL_ID;
+
 async function sendProductAlert(product) {
   const channel = await client.channels.fetch(CHANNEL_ID);
 
@@ -36,6 +38,7 @@ async function sendProductAlert(product) {
       `URL: ${product.url}`
   });
 }
+
 client.once("clientReady", async () => {
   console.log("PokemonTrackerV2 Online");
 
@@ -44,13 +47,10 @@ client.once("clientReady", async () => {
     name: "Pokemon Black Bolt ETB",
     url: "https://example.com"
   });
-});
-  console.log("PokemonTrackerV2 Online");
 
   // Target Scanner
   // Sam's Scanner
   // Costco Scanner
-
 });
 
 client.login(process.env.DISCORD_TOKEN);
